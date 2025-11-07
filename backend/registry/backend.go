93c86c128f6786e0f546eba5c337201be877457e
Alex <logsdonalex11@gmail.com>
2025-11-07 17:49:30 -0500

    fix: some clients (kubernetes) dont GET /v2/ before trying to fetch the manifest, and so need the www-authenticate handling done on other endpoints

--- backend/registry/backend.go
+++ backend/registry/backend.go
@@ -116,29 +116,55 @@ func (r *Registry) Store(ctx context.Context, container *dagger.Container, clien
 
 // Manifest implements backend.Backend.
 func (b *Registry) Manifest(_ context.Context, name string, reference digest.Digest) (http.Handler, error) {
-	return b.proxy(name, "manifests", reference)
+	return b.proxy("", "/v2", name, "manifests", reference.String()), nil
 }
 
 // Blob implements backend.Backend.
 func (b *Registry) Blob(_ context.Context, name string, reference digest.Digest) (http.Handler, error) {
-	return b.proxy(name, "blobs", reference)
+	return b.proxy("", "/v2", name, "blobs", reference.String()), nil
 }
 
 // Root implements backend.AuthBackend.
 func (b *Registry) Root(context.Context) (http.Handler, error) {
+	return b.proxy("", "/v2"), nil
+}
+
+// Token implements backend.AuthBackend.
+func (b *Registry) Token(context.Context) (http.Handler, error) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := logutil.SloggerFrom(r.Context())
+		q := r.URL.Query()
+		// NB: The incoming scope is "repository:<name>:pull". We specifically do not
+		// append <name> to b.Repository here in case b.Repository/<name> doesn't already exist,
+		// which causes ghcr to 403 the before we get to create b.Repository/<name> for the user.
+		// Surprisingly, this works.
+		scope := "repository:" + b.Repository + ":pull"
+		log.Debug("scope", "before", q.Get("scope"), "after", scope)
+		q.Set("scope", scope)
+		b.proxy(q.Encode(), b.TokenPath).ServeHTTP(w, r)
+	}), nil
+}
+
+func (b *Registry) proxy(query string, elem ...string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log := logutil.SloggerFrom(r.Context())
 
-		req, err := http.NewRequestWithContext(r.Context(), r.Method, b.getURL().JoinPath("/v2/").String(), nil)
+		u := b.getURL(elem...)
+		u.RawQuery = query
+
+		req, err := http.NewRequestWithContext(r.Context(), r.Method, u.String(), nil)
 		if err != nil {
+			log.Error(err.Error())
 			http.Error(w, err.Error(), httputil.HTTPStatusCode(err))
 			return
 		}
+		req.Header = r.Header.Clone()
 
 		w.Header().Set("X-Redirected", req.URL.String())
 
-		res, err := http.DefaultClient.Do(req)
+		res, err := http.DefaultTransport.RoundTrip(req)
 		if err != nil {
+			log.Error(err.Error())
 			http.Error(w, err.Error(), httputil.HTTPStatusCode(err))
 			return
 		}
@@ -166,84 +192,10 @@ func (b *Registry) Root(context.Context) (http.Handler, error) {
 			w.Header().Set("Www-Authenticate", rewrittenWwwAuth)
 		}
 
-		w.WriteHeader(res.StatusCode)
-		_, _ = io.Copy(w, res.Body)
-	}), nil
-}
-
-// Token implements backend.AuthBackend.
-func (b *Registry) Token(context.Context) (http.Handler, error) {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log := logutil.SloggerFrom(r.Context())
-		q := r.URL.Query()
-		// NB: The incoming scope is "repository:<name>:pull". We specifically do not
-		// append <name> to b.Repository here in case b.Repository/<name> doesn't already exist,
-		// which causes ghcr to 403 the before we get to create b.Repository/<name> for the user.
-		// Surprisingly, this works.
-		scope := "repository:" + b.Repository + ":pull"
-		log.Debug("scope", "before", q.Get("scope"), "after", scope)
-		q.Set("scope", scope)
-
-		req, err := http.NewRequestWithContext(r.Context(), r.Method, fmt.Sprintf("%s?%s", b.getTokenURL().String(), q.Encode()), nil)
-		if err != nil {
-			log.Error(err.Error())
-			http.Error(w, err.Error(), httputil.HTTPStatusCode(err))
-			return
-		}
-
-		w.Header().Set("X-Redirected", req.URL.String())
-
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			log.Error(err.Error())
-			http.Error(w, err.Error(), httputil.HTTPStatusCode(err))
-			return
-		}
-		defer res.Body.Close()
-
-		for k, v := range res.Header {
-			for _, vv := range v {
-				w.Header().Add(k, vv)
-			}
-		}
-
-		w.WriteHeader(res.StatusCode)
-		_, _ = io.Copy(w, res.Body)
-	}), nil
-}
-
-func (b *Registry) proxy(name, api string, reference digest.Digest) (http.Handler, error) {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log := logutil.SloggerFrom(r.Context())
-
-		req, err := http.NewRequestWithContext(r.Context(), r.Method, b.getURL("/v2", b.Repository, name, api, reference.String()).String(), nil)
-		if err != nil {
-			log.Error(err.Error())
-			http.Error(w, err.Error(), httputil.HTTPStatusCode(err))
-			return
-		}
-		req.Header = r.Header.Clone()
-
-		w.Header().Set("X-Redirected", req.URL.String())
-
-		res, err := http.DefaultTransport.RoundTrip(req)
-		if err != nil {
-			log.Error(err.Error())
-			http.Error(w, err.Error(), httputil.HTTPStatusCode(err))
-			return
-		}
-		defer res.Body.Close()
-
-		for k, v := range res.Header {
-			for _, vv := range v {
-				w.Header().Add(k, vv)
-			}
-		}
-
 		// Hopefully this is a redirect so we don't have to proxy massive blobs.
 		w.WriteHeader(res.StatusCode)
 		_, _ = io.Copy(w, res.Body)
-	}), nil
+	})
 }
 
 func (b *Registry) getURL(elem ...string) *url.URL {
